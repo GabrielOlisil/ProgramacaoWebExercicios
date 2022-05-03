267233c7e2be29c2df5bc1b6ea91dfc7dea2137b
Gabriel de Oliveira Silva <gabriel.oliveira.s177@gmail.com>
2022-05-03 17:20:52 -0400

    criando classes js

---
+++ EX13/nomes.js
@@ -0,0 +1,6 @@
+var nomes = ["Joao", "pepe", "Olga", "Olgabriel"];
+
+
+for (var i = 0; i < nomes.length; i++){
+    alert(nomes[i]);
+}
---
+++ EX13/nomesVazios.js
@@ -0,0 +1,7 @@
+var numeros = [];
+
+numeros.push(1);
+numeros.push(2);
+numeros.push(2);
+
+alert(numeros.length);
