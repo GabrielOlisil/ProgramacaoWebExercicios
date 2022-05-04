dba2ae37d4a07e21fb7c09e5d31a27442f58f15e
Gabriel de Oliveira Silva <gabriel.oliveira.s177@gmail.com>
2022-05-04 15:27:44 -0400

    atualizações no sript ex 14

--- EX14A19/EX14/script.js
+++ EX14A19/EX14/script.js
@@ -14,8 +14,19 @@ function NumerosPredefinidos(numero1, numero2){
 }
 
 function EscolhaNumero() {
-    var numero1 = parseInt(prompt("Insira o Primeiro Número"));
-    var numero2 = parseInt(prompt("Insira o Segundo Número"));
-    resposta = numero1 + numero2;
-    alert(resposta);
+    var numero1 = prompt("Insira o Primeiro Número");
+    var numero2 = prompt("Insira o Segundo Número");
+    
+    
+    if (numero1 == ""){
+        numero1 == 0;
+    }
+    
+        
+    if (numero2 == ""){
+        numero2 == 0;
+    }
+    
+    resposta = parseInt(numero1) + parseInt(numero2);
+    alert(resposta);   
 }
