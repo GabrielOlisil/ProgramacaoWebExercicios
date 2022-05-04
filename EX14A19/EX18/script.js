76a4bf0e5ca85f68db4488ce0dd8edbfcc8f0506
Gabriel de Oliveira Silva <gabriel.oliveira.s177@gmail.com>
2022-05-04 17:13:11 -0400

    update exercicios

---
+++ EX14A19/EX18/script.js
@@ -0,0 +1,4 @@
+function Comeco(){
+    resposta = prompt("Olha só, meu campo de resposta é editável, voc~e consegue digitar!!!!");
+    alert("Oi, é a alert denovo, não me chame de intrometida, só estou aqui para mostrar o que você escreveu na função prompt!!!! A propósito, voce escreveu:\n \t" + resposta);
+}
