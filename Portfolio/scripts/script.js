3b51ba3341f4f878dcefe941b070385746ee0d8c
Gabriel de Oliveira Silva <gabriel.oliveira.s177@gmail.com>
2022-06-14 17:19:49 -0400

    criação carrossel

--- Portfolio/scripts/script.js
+++ Portfolio/scripts/script.js
@@ -1,5 +1,48 @@
 (function(){
 
+    const ferramentas = document.getElementById("ferramentas-conhecidas");
+    const ferramentasItens = document.querySelectorAll("#ferramentas-conhecidas > div");
+
+    let counter = 0
+    console.log(ferramentasItens.length)
+
+    for(let i = 1; i < ferramentasItens.length; i++){
+        ferramentasItens[i].remove()
+    }
+
+
+    const btnPassar = document.getElementById("btnProximo")
+    const btnVoltar = document.getElementById("btnAnterior")
+
+
+    btnVoltar.addEventListener('click', () => {
+        ferramentasItens[counter].remove()
+
+        counter--
+        
+        counter =  counter >= 0? counter : 3
+
+        ferramentas.insertBefore(ferramentasItens[counter], ferramentas.firstChild)
+
+
+
+    })
+
+
+
+    btnPassar.addEventListener('click', () => {
+        ferramentasItens[counter].remove()
+
+        counter++
+        
+        counter =  counter < 4? counter : 0
+
+        ferramentas.insertBefore(ferramentasItens[counter], ferramentas.firstChild)
+
+
+
+    })
+
     
 
 
